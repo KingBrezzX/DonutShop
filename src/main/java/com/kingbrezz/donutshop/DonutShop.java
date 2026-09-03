a1799328a60e761433eedbce87ddd4f0756371ca
KingBrezzz <bobnbb046@gmail.com>
2026-09-03 12:35:17 +0700

    Implement DonutShop plugin with economy and shop management

---
+++ src/main/java/com/kingbrezz/donutshop/DonutShop.java
@@ -0,0 +1,91 @@
+package com.kingbrezz.donutshop;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DonutShop extends JavaPlugin {
+
+    private Economy economy;
+    private ShopManager shopManager;
+    private LanguageManager languageManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        languageManager = new LanguageManager(this);
+        languageManager.load();
+
+        if (!setupEconomy()) {
+            getLogger().severe("Vault economy was not found.");
+            getLogger().severe("DonutShop requires Vault and an economy provider.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        shopManager = new ShopManager(this, economy);
+        shopManager.load();
+
+        ShopCommand shopCommand = new ShopCommand(this);
+
+        getCommand("shop").setExecutor(shopCommand);
+        getCommand("shop").setTabCompleter(shopCommand);
+
+        getCommand("shopedit").setExecutor(new ShopEditCommand(this));
+        getCommand("shopedit").setTabCompleter(new ShopEditCommand(this));
+
+        getCommand("donutshop").setExecutor(new DonutShopCommand(this));
+        getCommand("donutshop").setTabCompleter(new DonutShopCommand(this));
+
+        getServer().getPluginManager().registerEvents(
+                new ShopListener(this),
+                this
+        );
+
+        getLogger().info("DonutShop enabled successfully.");
+    }
+
+    @Override
+    public void onDisable() {
+        if (shopManager != null) {
+            shopManager.save();
+        }
+
+        getLogger().info("DonutShop disabled.");
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> provider =
+                getServer().getServicesManager().getRegistration(Economy.class);
+
+        if (provider == null) {
+            return false;
+        }
+
+        economy = provider.getProvider();
+        return economy != null;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+
+    public ShopManager getShopManager() {
+        return shopManager;
+    }
+
+    public LanguageManager getLanguageManager() {
+        return languageManager;
+    }
+
+    public void reloadPlugin() {
+        reloadConfig();
+        languageManager.load();
+        shopManager.load();
+    }
+}
